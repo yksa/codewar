43c345a7da012842e69b40680f1e3e569acabb5a
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-08 00:42:26 +0630

    ticket solution

---
+++ tickets.js
@@ -0,0 +1,40 @@
+// The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+// Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+// Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+
+// Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+// tickets([25, 25, 50]) // => YES 
+// tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
+// tickets([25, 25, 50, 50, 100]) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+
+const tickets = peopleInLine => {
+    let bal25 = 0;
+    let bal50 = 0;
+    for (i in peopleInLine) {
+        if (peopleInLine[i] === 25) {
+            bal25 += 1;
+        } else if (peopleInLine[i] === 50) {
+            if (bal25 === 0) return "NO";
+            else {
+                bal25 -= 1;
+                bal50 += 1;
+            }
+        } else {
+            if (bal50 > 0) {
+                if (bal25 > 0) {
+                    bal50 -= 1;
+                    bal25 -= 1;
+                } else return "NO";
+            } else if (bal25 > 2) {
+                bal25 -= 3;
+            } else return "NO";
+        }
+    }
+    return "YES";
+};
+
+let peopleInLine = [25, 25, 25, 100, 25, 25, 25, 100, 25, 25, 25, 100, 25, 25, 25, 100, 25, 50, 25, 100];
+let ans = tickets(peopleInLine);
+console.log(ans);
